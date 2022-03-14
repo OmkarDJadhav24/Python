58c2f2fbfb0b33d121c5766fec519f21c30bd1dc
OmkarDJadhav24 <101233431+OmkarDJadhav24@users.noreply.github.com>
2022-03-14 10:59:25 +0530

    Add files via upload

---
+++ convert.py
@@ -0,0 +1,17 @@
+def main():
+	no=11
+	x=str(no)
+	xx=oct(no)
+	xxx=hex(no)
+	print("No",no)
+	print("No in str",x)
+	print("No in oct",xx)
+	print("No in hex",xxx)
+	print("no",type(no))
+	print("x",type(x))
+	print("xx",type(xx))
+	print("xxx",type(xxx))
+
+
+if __name__=="__main__":
+	main()
